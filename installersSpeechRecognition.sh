fd156cffe1d3788ccb61d22abfd530adb57a0375
root <marco.maigua@nobleprog.eu>
2020-02-13 15:50:41 -0500

    more scripts

--- installersSpeechRecognition.sh
+++ installersSpeechRecognition.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6
+sudo apt-get install alsa alsa-tools
+sudo apt install python3-pip
+sudo apt-get install libpulse-dev
+sudo apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev
 sudo apt-get install -qq python python-dev python-pip build-essential swig libpulse-dev
-sudo pip install pocketsphinx
+sudo pip3 install pocketsphinx
 
