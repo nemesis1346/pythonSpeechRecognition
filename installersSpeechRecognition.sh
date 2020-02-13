792870d8086ba9dd87d41f6ebd0eaf90fff2a861
root <marco.maigua@nobleprog.eu>
2020-02-13 17:22:57 -0500

    adding dependencies

--- installersSpeechRecognition.sh
+++ installersSpeechRecognition.sh
@@ -7,7 +7,8 @@ sudo apt-get install python3.6
 sudo apt-get install alsa alsa-tools
 sudo apt install python3-pip
 sudo apt-get install libpulse-dev
+sudo apt install libasound2-dev
 sudo apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev
 sudo apt-get install -qq python python-dev python-pip build-essential swig libpulse-dev
 sudo pip3 install pocketsphinx
-
+sudo pip3 install SpeechRecognition
